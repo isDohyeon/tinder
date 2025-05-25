ffc5f67304115cce07d56531b6d94d4bc4fe7a42
isDohyeon <eun041212@gmail.com>
2025-05-25 23:57:33 +0900

    feat: 로그인 기능 구현

--- app/src/main/java/hnu/multimedia/tinder/auth/IntroActivity.kt
+++ app/src/main/java/hnu/multimedia/tinder/auth/IntroActivity.kt
@@ -3,6 +3,9 @@ package hnu.multimedia.tinder.auth
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import hnu.multimedia.tinder.MainActivity
 import hnu.multimedia.tinder.databinding.ActivityIntroBinding
 
@@ -21,9 +24,26 @@ class IntroActivity : AppCompatActivity() {
         }
 
         binding.buttonLogin.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
+
+            val email = binding.editTextEmail.text.toString()
+            val password = binding.editTextPassword.text.toString()
+
+            if (email.isEmpty()) {
+                Snackbar.make(binding.root, "이메일을 입력해주세요.", Snackbar.LENGTH_LONG).show()
+            } else if (password.isEmpty()) {
+                Snackbar.make(binding.root, "패스워드를 입력해주세요.", Snackbar.LENGTH_LONG).show()
+            } else {
+                Firebase.auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            Snackbar.make(binding.root, "로그인에 실패했습니다", Snackbar.LENGTH_LONG).show()
+                        }
+                    }
+            }
         }
     }
 }
