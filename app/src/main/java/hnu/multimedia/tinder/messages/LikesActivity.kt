f4440c706407db9336e504f46d9da35f6eac4739
isDohyeon <eun041212@gmail.com>
2025-06-01 21:45:07 +0900

    feat: 좋아요 한 사람의 uid를 통해 그 사람의 정보를 UserModel로 가져오며 로그로 출력하는 기능 구현

--- app/src/main/java/hnu/multimedia/tinder/messages/LikesActivity.kt
+++ app/src/main/java/hnu/multimedia/tinder/messages/LikesActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import hnu.multimedia.tinder.auth.UserModel
 import hnu.multimedia.tinder.databinding.ActivityLikesBinding
 import hnu.multimedia.tinder.util.FirebaseRef
 
@@ -13,6 +14,7 @@ class LikesActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityLikesBinding.inflate(layoutInflater) }
     private val likes = mutableListOf<String>()
+    private val likedUsers = mutableListOf<UserModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,9 +33,30 @@ class LikesActivity : AppCompatActivity() {
                 }
                 Log.d("LikesActivity", "getLikes: ${likes}")
 //                binding.cardStackView.adapter?.notifyDataSetChanged()
+                getLikedUsers()
             }
             override fun onCancelled(error: DatabaseError) {}
         }
         FirebaseRef.likes.child(FirebaseRef.currentUserId).addValueEventListener(postListener)
+    }
+
+    private fun getLikedUsers() {
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                likedUsers.clear()
+                for (dataModel in snapshot.children) {
+                    val value = dataModel.getValue(UserModel::class.java)
+                    value?.let {
+                        if (likes.contains(it.uid)) {
+                            likedUsers.add(it)
+                        }
+                    }
+                }
+                Log.d("LikesActivity", "getLikedUsers: ${likedUsers}")
+//                binding.cardStackView.adapter?.notifyDataSetChanged()
+            }
+            override fun onCancelled(error: DatabaseError) {}
+        }
+        FirebaseRef.users.addValueEventListener(postListener)
     }
 }
